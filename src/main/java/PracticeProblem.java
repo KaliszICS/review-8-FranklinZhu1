0ac4fcd011c3d2f0fa2d2cc8582efbaa1b6eebed
franklin-zhu-ics4u1-02-25 <123124410+FranklinZhu1@users.noreply.github.com>
2025-02-20 04:06:00 +0000

    Create solution methods

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,11 +1,39 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
-
+	public static String pluralize(String word) {
+		switch(word.length()) {
+			case 0:
+				return "s";
+			case 1:
+				if (word.equals("y")) return "ies";
+				else return "s";
+			case 2:
+				if (word.equals("ey")) return "eys";
+				else if (word.charAt(1) == 'y') return "ies";
+				else return "s";
+			default: // technically redundant default statement
+				if (word.substring(word.length() - 2).equalsIgnoreCase("ey")) return "eys";
+				else if (word.substring(word.length() - 1).equalsIgnoreCase("y")) return "ies";
+				else if (word.substring(word.length() - 3).equalsIgnoreCase("ife")) return "ives";
+				else return "s";
+		}
 	}
 
 	public static int min(int num1, int num2, int num3) {
-		return num1;
+		return Math.min(Math.min(num1, num2), num3);
+	}
+
+	public static boolean isLeapYear(int num1) {
+		if (num1%4 == 0) {
+			if (num1%100 == 0) {
+				if (num1%400 == 0) {
+					return true;
+				}
+				else return false;
+			}
+			else return true;
+		}
+		else return false;
 	}
 
 }
